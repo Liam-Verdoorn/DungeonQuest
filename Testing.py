73091611e43f7f67e4b343e3bc37ef2cb92745ee
liam <liam@server1.servicenet.nl>
2019-03-26 10:28:16 -0400

    Updated test file

--- Testing.py
+++ Testing.py
@@ -1,26 +1,33 @@
-stat = 30
-Max_Upgrades = 124
-calc = 1
+# stat = 30
+# maxUpgrades = 124
+# calc = 1
+#
+# for i in range(upgrades):
+    # calc = int(stat * 0.05)
+    # if calc >= 10:
+        # calc = 10
+    # stat = stat + calc
 
-for i in range (upgrades):
-    calc = int(stat * 0.05)
-    if calc >= 10:
-        calc = 10
-    stat = stat + calc
-    print(stat)
+    # 1. 100    + 56 = 156
+    # 2. 156    + (56 * 1.0618) = 215 from 2/3 is 215 and from we don't have 3/4 4/5 is 344 from upgrade 2 to upgrade 3 costs is what it means
+    # 3. 215    + (56 * 1.0618) = 274  is the line itself even correct would it output 274
+    # 4. 274    + (56 * 1.0618) = 333  print(274.4608    + (56 * 1.242))
+    
 
-start = 100
-upgrade = 56
-c = 0
-d = 0
-e = 0
-f = 0
-upgrades = input()
+startingCost = 100 # add upgradeCost and multiply by 1.0618
+upgradeCost = 56
+upgradeLevel = 0
+totalCost = 0
+upgrades = 30 #user inputs their current upgrade level here
 
 for i in range(upgrades):
-    upgrade = int(up*1.0618)
-    if b >= 500:
-        b = 500
-    a = a + b
-    c = c + a
-    d = d + 1
+    upgradeCost = int(round((startingCost + upgradeCost) * 1.0618))
+
+    if upgradeCost >= 500:
+        upgradeCost = 500
+    startingCost = startingCost + upgradeCost
+    totalCost = startingCost + upgradeCost
+    currentUpgradeLevel = currentUpgradeLevel + 1
+    if currentUpgradeLevel == (upgrades):
+        currentUpgradeLevel = currentUpgradeLevel + totalCost - currentUpgradeLevel
+    print(totalCost)
